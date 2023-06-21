2a8f6e395d6cc6027684ed60b7439cf7cded58bf
qiuyiyang0521 <qiuyiyang0521@gmail.com>
2023-06-21 19:56:11 +0800

    提交

--- search.py
+++ search.py
@@ -1,1 +1,10 @@
-def linear_search():
+def linear_search(li, value):
+    for index, val in enumerate(li):
+        if val == value:
+            return index
+    else:
+        return None
+
+
+li = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(linear_search(li, 5))
