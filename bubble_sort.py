9bc09194f96cc805039083266b504a9d23d5ed01
qiuyiyang0521 <qiuyiyang0521@gmail.com>
2023-07-30 17:27:05 +0800

    提交

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def bubble_sort(li):
+    for x in range(len(li) - 1): # 1趟
+        for y in range(len(li) - x - 1):
+            if li[y] > li[y + 1]:
+                li[y], li[y + 1] = li[y + 1], li[y]
+    return li
+
+
+li = [5.9,8,6,3,2,4,8,6,5]
+li = bubble_sort(li)
+print(li)
