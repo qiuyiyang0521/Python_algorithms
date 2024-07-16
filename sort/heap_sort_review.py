ef3232a827077572abad1a566c83c95c11d84494
qiuyiyang0521 <qiuyiyang0521@gmail.com>
2024-07-16 18:09:50 +0800

    更新quick_sort_review.py和heap_sort_review.py

--- sort/heap_sort_review.py
+++ sort/heap_sort_review.py
@@ -1,15 +1,11 @@
-from timer import timer
-
-
-def heapify(li: list, start_index: int, end_index: int) -> list:
+def heapify(li: list, start_index: int, end_index: int) -> None:
     """
     给指定范围的列表堆化（向下调整）
     :param li: 需要堆化的列表
     :param start_index: 堆化范围根节点的索引
     :param end_index: 堆化范围最后一个节点的索引
-    :return: 范围内堆化后的列表
+    :return: None
     """
-    li = li.copy()
     index = start_index
     left = 2 * start_index + 1
     right = 2 * start_index + 2
@@ -32,27 +28,20 @@ def heapify(li: list, start_index: int, end_index: int) -> list:
         else:
             break
 
-    return li
 
-
-def build_heap(li: list) -> list:
+def build_heap(li: list) -> None:
     """
     构建堆
     :param li: 需要构建堆的列表:
-    :return: 堆结构的列表
+    :return: None
     """
-    li = li.copy()
     last_parent_node_index = (len(li) - 2) // 2
     for index in range(last_parent_node_index, -1, -1):
-        li = heapify(li, index, len(li) - 1)
-
-    return li
+        heapify(li, index, len(li) - 1)
 
 
-def heap_sort(li: list) -> list:
-    li = li.copy()
-    li = build_heap(li)
+def heap_sort(li: list) -> None:
+    build_heap(li)
     for i in range(len(li) - 1, 0, -1):
         li[0], li[i] = li[i], li[0]
-        li = heapify(li, 0, i - 1)
-    return li
+        heapify(li, 0, i - 1)
--- sort/quick_sort_review.py
+++ sort/quick_sort_review.py
@@ -1,3 +1,6 @@
+from random import shuffle
+
+
 def swap(li: list, value1_index: int, value2_index: int) -> None:
     """
     交换列表中两个值
@@ -9,25 +12,44 @@ def swap(li: list, value1_index: int, value2_index: int) -> None:
     li[value1_index], li[value2_index] = li[value2_index], li[value1_index]
 
 
-def partition(data: list, start: int, end: int) -> None:
+def partition(data: list, start: int, end: int) -> int:
     """
     快速排序中的分区函数
     :param data: 需要分区的列表
     :param start: 分区范围开始的索引
     :param end: 分区范围结束的所引
-    :return: 没有返回值
+    :return: 返回基准值的下标
     """
     base_value = data[start]
     i, j = start, end
 
     while i < j:  # not i > 9
-        while data[i] <= base_value and i < j:
-            i += 1
         while data[j] >= base_value and i < j:
             j -= 1
-        swap(data, i, j)
+        while data[i] <= base_value and i < j:
+            i += 1
+        if i < j:
+            swap(data, i, j)
 
-    else:
-        # 此时i正确，即data[i]>base_value，而j=i，所以li[j-1]<base_value
-        j -= 1  # 将j-1，使j符合条件
-        swap(data, start, j)
+    swap(data, start, i)
+
+    return i  # 返回基准值的下标
+
+
+def quick_sort(li: list, left: int, right: int) -> None:
+    """
+    快速排序
+    :param li: 被排序的列表
+    :return: 没有返回值
+    """
+
+    if left < right:
+        base_index = partition(li, left, right)
+        quick_sort(li, left, base_index - 1)
+        quick_sort(li, base_index + 1, right)
+
+if __name__ == '__main__':
+    li = list(range(1, 21))
+    shuffle(li)
+    quick_sort(li, 0, len(li) - 1)
+    print(li)
